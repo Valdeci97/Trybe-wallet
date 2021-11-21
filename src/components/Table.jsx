770e2dedcba298c7f0c44eaf925fc552f4e147d6
Valdeci <moura97.valdeci@gmail.com>
2021-11-20 21:41:38 -0300

    Organiza componentes e requisito 8

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,12 @@
+/* eslint-disable react/no-multi-comp */
 import React from 'react';
-import { connect } from 'react-redux';
-
-import PropTypes from 'prop-types';
-import { removeChargeAction } from '../actions';
+import TableData from './TableData';
 
 class Table extends React.Component {
   render() {
-    const { expenses, removeExpenses } = this.props;
     return (
-      <table>
-        <table>
+      <>
+        <thead>
           <tr>
             <th>Descrição</th>
             <th>Tag</th>
@@ -21,52 +18,13 @@ class Table extends React.Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
-        </table>
-        <table>
-          {expenses.map((expense) => (
-            <tr key={ expense.id }>
-              <td>{ expense.description }</td>
-              <td>{ expense.tag }</td>
-              <td>{ expense.method }</td>
-              <td>{ expense.value }</td>
-              <td>{ expense.exchangeRates[expense.currency].name.split('/')[0] }</td>
-              <td>
-                {Number(expense.exchangeRates[expense.currency].ask)
-                  .toFixed(2)}
-              </td>
-              <td>
-                {Number(expense.exchangeRates[expense.currency].ask * expense.value)
-                  .toFixed(2)}
-              </td>
-              <td>Real</td>
-              <button
-                type="button"
-                data-testid="delete-btn"
-                onClick={ () => removeExpenses(expense.id) }
-              >
-                Excluir
-              </button>
-            </tr>
-          ))}
-        </table>
-      </table>
+        </thead>
+        <TableData />
+      </>
     );
   }
 }
 
-const mapStateToProps = ({ wallet }) => ({
-  expenses: wallet.expenses,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  removeExpenses: (payload) => dispatch(removeChargeAction(payload)),
-});
-
-Table.propTypes = {
-  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
-  removeExpenses: PropTypes.func.isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Table);
+export default Table;
 
 // Fonte sobre como montar tabela com react: https://www.youtube.com/watch?v=R6sRxQ9nd0A
---
+++ src/components/TableData.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { removeChargeAction } from '../actions';
+
+class TableData extends React.Component {
+  render() {
+    const { expenses, removeExpenses } = this.props;
+    return (
+      <table>
+        {expenses.map((expense) => (
+          <tr key={ expense.id }>
+            <td>{ expense.description }</td>
+            <td>{ expense.tag }</td>
+            <td>{ expense.method }</td>
+            <td>{ expense.value }</td>
+            <td>{ expense.exchangeRates[expense.currency].name.split('/')[0] }</td>
+            <td>
+              {Number(expense.exchangeRates[expense.currency].ask)
+                .toFixed(2)}
+            </td>
+            <td>
+              {Number(expense.exchangeRates[expense.currency].ask * expense.value)
+                .toFixed(2)}
+            </td>
+            <td>Real</td>
+            <td>
+              <button
+                type="button"
+                data-testid="delete-btn"
+                onClick={ () => removeExpenses(expense.id) }
+              >
+                Excluir
+              </button>
+            </td>
+          </tr>
+        ))}
+      </table>
+    );
+  }
+}
+
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  removeExpenses: (payload) => dispatch(removeChargeAction(payload)),
+});
+
+TableData.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+  removeExpenses: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TableData);
